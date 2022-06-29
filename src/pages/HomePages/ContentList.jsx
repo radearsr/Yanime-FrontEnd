888830da1127d9cac26c04e6f449dacbe00f1b3f
radearsr <radea.surya1@gmail.com>
2022-06-29 17:43:32 +0700

    experiment useeffect

--- src/pages/HomePages/ContentList.jsx
+++ src/pages/HomePages/ContentList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { Container } from "react-bootstrap";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
