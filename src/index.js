1c063962c87f1799e7b5b1bd2ecfd08d3514d928
radearsr <radea.surya1@gmail.com>
2022-07-08 11:17:00 +0700

    new folder to new route

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import VideoUI from "./ExperimentComp/VideoUI";
-// import Endpoint from "./ExperimentComp/EndPoint";
 import { BrowserRouter } from "react-router-dom";
-import App from "./pages/App";
+import App from "./page-new/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <VideoUI />
-//   </React.StrictMode>
-// );
--- src/pages/HomePages/ContentList.jsx
+++ src/pages/HomePages/ContentList.jsx
@@ -13,50 +13,48 @@ const ContentList = (props) => {
   }, []);
 
   return (
-    <div className="list-wrapper">
-      <Container>
-        <header>
-          <h1 className="section-title">{props.titleContentList}</h1>
-          <a href={"/"} className="link-more">
-            Lebih banyak
-          </a>
-        </header>
-        <main>
-          <Swiper
-            className="contents"
-            slidesPerView={3}
-            spaceBetween={10}
-            breakpoints={{
-              992: {
-                slidesPerView: 7,
-                spaceBetween: 20,
-              },
-              768: {
-                slidesPerView: 4,
-                spaceBetween: 15,
-              },
-            }}
-          > 
-            {animes.map((anime, index) => (
-              <SwiperSlide key={index}>
-                <a 
-                  href={`/streaming/${anime.title.split(" ").join("-").toLowerCase()}${anime.type === "series" ? "-eps-1" : ""}`}
-                  className="content-item"
-                >
-                  <div className="poster-content-item d-flex">
-                    <img
-                      src={anime.poster}
-                      alt={anime.title}
-                      className="img-fluid"
-                    />
-                  </div>
-                  <p>{anime.title}</p>
-                </a>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </main>
-      </Container>
+    <div className="list-wrapper">  
+      <header>
+        <h1 className="section-title">{props.titleContentList}</h1>
+        <a href={"/"} className="link-more">
+          Lebih banyak
+        </a>
+      </header>
+      <main>
+        <Swiper
+          className="contents"
+          slidesPerView={3}
+          spaceBetween={10}
+          breakpoints={{
+            992: {
+              slidesPerView: 7,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 15,
+            },
+          }}
+        > 
+          {animes.map((anime, index) => (
+            <SwiperSlide key={index}>
+              <a 
+                href={`/streaming/${anime.title.split(" ").join("-").toLowerCase()}${anime.type === "series" ? "-eps-1" : ""}`}
+                className="content-item"
+              >
+                <div className="poster-content-item d-flex">
+                  <img
+                    src={anime.poster}
+                    alt={anime.title}
+                    className="img-fluid"
+                  />
+                </div>
+                <p>{anime.title}</p>
+              </a>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </main>
     </div>
   );
 };
