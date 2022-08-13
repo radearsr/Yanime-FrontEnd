9d5e6edbc00701ca8e70cf4c47473595849f0f22
barianbarbar <barianbarbar99@gmail.com>
2022-08-13 16:07:09 +0700

    hapus commentar, ubah struktur pada video streaming

--- src/pages/VideoStreaming/index.jsx
+++ src/pages/VideoStreaming/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import {
+  Container,
   Row,
   Col,
 } from "react-bootstrap";
@@ -13,10 +14,17 @@ import "./videoStreaming.css";
 
 const VideoStreaming = () => {
   return (
-    <div className="wrapper-video-streaming">
-      <Row className="g-0 justify-content-evenly">
-        <Col xs={12} lg={8}>
-          <ReactPlayer style={{marginTop: "-2px"}} width="100%" height="100%" url={"https://first-server-uqgwz.run-us-west2.goorm.io/api/video/kimi-no-nawa"}  controls />
+    <Container fluid="lg">
+      <Row className="g-0 justify-content-evenly wrapper-streaming-video">
+        <Col xs={12} lg={8} className="g-0">
+          <div className="video-wrapper">
+            <ReactPlayer
+              width={"100%"}
+              height={"100%"}
+              url={"https://first-server-uqgwz.run-us-west2.goorm.io/api/video/kimi-no-nawa"}  
+              controls={true} 
+            />
+          </div>
         </Col>
         <Col xs={12} lg={3}>
           <h3 className="title">Daftar Episode</h3>
@@ -39,7 +47,7 @@ const VideoStreaming = () => {
       <AboutAnime
         text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio quia repudiandae libero, accusamus in repellendus quo temporibus autem enim, quos corrupti explicabo aperiam! Tempora eligendi a reiciendis, quidem ducimus similique assumenda culpa dolore illo voluptates et corporis inventore voluptate? Dolorem.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio quia repudiandae libero, accusamus in repellendus quo temporibus autem enim, quos corrupti explicabo aperiam! Tempora eligendi a reiciendis, quidem ducimus similique assumenda culpa dolore illo voluptates et corporis inventore voluptate? Dolorem."
       />
-    </div>
+    </Container>
   );
 };
 
