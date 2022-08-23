4bf5d3bfd0129e78f19643ab660a9a150f17c945
barianbarbar <barianbarbar99@gmail.com>
2022-08-23 20:53:39 +0700

    add new feature scroll to episode

--- src/components/Atoms/EpisodeItem/index.jsx
+++ src/components/Atoms/EpisodeItem/index.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./episodeItem.css";
 
 const EpisodeItem = ({number, linkVideo, activePage}) => {
+  const epsRef = useRef(null);
   const navigate = useNavigate();
   const curActivePage = parseFloat(activePage) === parseFloat(number) ? "active" : "";
+  const curRef = parseFloat(activePage) === parseFloat(number) ? epsRef : null;
+
+  useEffect(() => {
+    if (epsRef.current != null) {
+      epsRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  });
 
   return (
-    <div className={`episode-item ${curActivePage}`} onClick={() => navigate(`/streaming/${linkVideo}`)}>
+    <div className={`episode-item ${curActivePage}`} onClick={() => navigate(`/streaming/${linkVideo}`)} ref={curRef}>
       <span className="number">{number}</span>
       <div className="group-label">
         <p className="label">Episode</p>
